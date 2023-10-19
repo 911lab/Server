0f9d26efffe1e78e2d566f4e5e4afc8c4b6551b4
JaeHyuk <gu9062@naver.com>
2023-10-19 21:37:20 +0900

    MAF Finish Commit

---
+++ src/main/java/com/example/bleLocationSystem/model/MAFilter.java
@@ -0,0 +1,52 @@
+package com.example.bleLocationSystem.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MAFilter {
+    int size;
+    List<Double> queue;
+    double avg;
+
+    double sum;
+    int num;
+    int tempNum;
+
+    int queueSize;
+
+    public MAFilter () {
+        size = 10;
+        queue = new ArrayList<Double>();
+        sum = 0;
+        avg = 0;
+        num = 0;
+    }
+
+    public double push(double rssi) {
+        sum = 0;
+        tempNum = num%10;
+
+        queueSize = queue.size();
+
+        if(queueSize < size) {
+            queue.add(rssi);
+        }
+        else {
+            queue.set(tempNum, rssi);
+            num++;
+        }
+
+        for(double r : queue) {
+            sum = sum+r;
+        }
+
+        if(queueSize < size) {
+            avg = sum / (queueSize + 1);
+        }
+        else {
+            avg = sum / queueSize;
+        }
+
+        return avg;
+    }
+}
