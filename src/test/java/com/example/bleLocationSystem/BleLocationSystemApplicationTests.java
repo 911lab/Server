6aadcc1310b32b1157ef4ab697b47e842cc01317
JaeHyuk <gu9062@naver.com>
2023-12-09 14:53:32 +0900

    jar File Finish Commit

--- src/test/java/com/example/bleLocationSystem/BleLocationSystemApplicationTests.java
+++ src/test/java/com/example/bleLocationSystem/BleLocationSystemApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.bleLocationSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BleLocationSystemApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
